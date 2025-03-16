d2002acc62850161b7c256de89d691f2f4aad686
yuvaraj9999 <yuvipragada123@gmail.com>
2025-03-16 02:41:31 +0000

    Updated Successful Code

--- src/App.js
+++ src/App.js
@@ -1,64 +1,25 @@
 import {Route, Switch, Redirect} from 'react-router-dom'
 
-import AboutJobItem from './component/AboutJobItem'
-import AllJobs from './component/AllJobs'
-import ProtectedRoute from './component/ProtectedRoute'
-
-import NotFound from './component/NotFound'
-import Home from './component/Home'
-
-import LoginForm from './component/LoginForm'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+import JobItemDetails from './components/JobItemDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
 
-// These are the lists used in the application. You can move them to any component needed.
-const employmentTypesList = [
-  {
-    label: 'Full Time',
-    employmentTypeId: 'FULLTIME',
-  },
-  {
-    label: 'Part Time',
-    employmentTypeId: 'PARTTIME',
-  },
-  {
-    label: 'Freelance',
-    employmentTypeId: 'FREELANCE',
-  },
-  {
-    label: 'Internship',
-    employmentTypeId: 'INTERNSHIP',
-  },
-]
-
-const salaryRangesList = [
-  {
-    salaryRangeId: '1000000',
-    label: '10 LPA and above',
-  },
-  {
-    salaryRangeId: '2000000',
-    label: '20 LPA and above',
-  },
-  {
-    salaryRangeId: '3000000',
-    label: '30 LPA and above',
-  },
-  {
-    salaryRangeId: '4000000',
-    label: '40 LPA and above',
-  },
-]
-
-// Replace your code here
 const App = () => (
-  <Switch>
-    <Route exact path="/login" component={LoginForm} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/jobs" component={AllJobs} />
-    <ProtectedRoute exact path="/jobs/:id" component={AboutJobItem} />
-    <Route exact path="/not-found" component={NotFound} />
-    <Redirect to="not-found" />
-  </Switch>
+  <>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
+    </Switch>
+  </>
 )
+
 export default App
